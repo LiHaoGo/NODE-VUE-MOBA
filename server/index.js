f466df6ba9ebc3da49023211c978026c88ce93fd
LeYi <1337741723@qq.com>
2021-08-30 17:05:01 +0800

    master

--- server/index.js
+++ server/index.js
@@ -7,10 +7,8 @@ app.use('/admin', express.static(__dirname + '/admin'))
 app.use('/', express.static(__dirname + '/web'))
 app.use('/uploads', express.static(__dirname + '/uploads'))
 require('./plugins/db')(app)
-
 require('./routes/admin')(app)
 require('./routes/web')(app)
-
 app.listen(3000,()=>{
     console.log("http://localhost:3000")
 })
