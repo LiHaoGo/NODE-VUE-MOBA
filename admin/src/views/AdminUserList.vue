c9d460e182bf9c01678e3e120e67a3916d8ddf30
LeYi <1337741723@qq.com>
2021-08-30 17:08:12 +0800

    部署前的修改提交

--- admin/src/views/AdminUserList.vue
+++ admin/src/views/AdminUserList.vue
@@ -6,8 +6,15 @@
       <el-table-column prop="username" label="用户名"> </el-table-column>
       <el-table-column fixed="right" label="操作" width="180">
         <template slot-scope="scope">
-          <el-button @click="$router.push(`/admin_users/edit/${scope.row._id}`)" type="primary" size="small">编辑</el-button>
-          <el-button @click="remove(scope.row)" type="danger" size="small">删除</el-button>
+          <el-button
+            @click="$router.push(`/admin_users/edit/${scope.row._id}`)"
+            type="primary"
+            size="small"
+            >编辑</el-button
+          >
+          <el-button @click="remove(scope.row)" type="danger" size="small"
+            >删除</el-button
+          >
         </template>
       </el-table-column>
     </el-table>
@@ -27,24 +34,24 @@ export default {
       const res = await this.$http.get("rest/admin_users");
       this.items = res.data;
     },
-    async remove(row){
+    async remove(row) {
       console.log(row);
-       this.$confirm(`是否确定要删除分类"${row.name}"?`, '提示', {
-          confirmButtonText: '确定',
-          cancelButtonText: '取消',
-          type: 'warning'
-        }).then(async () => {
-          // 代表用户一定点了删除，然后请求接口
-          const res = await this.$http.delete(`rest/admin_users/${row._id}`)
-          console.log('delete',res);
-          
-         this.$message({
-            type: 'success',
-            message: '删除成功!'
-          });
-          this.fetch()
-        })         
-    }
+      this.$confirm(`是否确定要删除分类"${row.name}"?`, "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning",
+      }).then(async () => {
+        // 代表用户一定点了删除，然后请求接口
+        const res = await this.$http.delete(`rest/admin_users/${row._id}`);
+        console.log("delete", res);
+
+        this.$message({
+          type: "success",
+          message: "删除成功!",
+        });
+        this.fetch();
+      });
+    },
   },
   created() {
     this.fetch();
